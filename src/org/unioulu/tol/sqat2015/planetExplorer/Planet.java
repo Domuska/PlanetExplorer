9b9b52e0f8d84382458004f7a9d310927e9cd7da
somename <someemail>
2015-10-23 11:52:17 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/Planet.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/Planet.java
@@ -2,10 +2,26 @@ package org.unioulu.tol.sqat2015.planetExplorer;
 
 public class Planet {
 
+	int x;
+	int y;
+	
 	
 	public Planet(int x, int y){
 		
-		
+		this.x = x;
+		this.y = y;
 	}
+
+
+	public int getX() {
+		return x;
+	}
+
+
+	public int getY() {
+		return y;
+	}
+	
+	
 	
 }
