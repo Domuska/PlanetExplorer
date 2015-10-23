fcf5527f4aa7e5b1548555597c2a566e91c0ff59
somename <someemail>
2015-10-23 12:39:10 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/Explorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/Explorer.java
@@ -2,4 +2,23 @@ package org.unioulu.tol.sqat2015.planetExplorer;
 
 public class Explorer {
 
+	private int x,y;
+	private String orientation; 
+	
+	public Explorer(int x,int y, String orientation){
+		
+		this.x = x;
+		this.y = y;
+		this.orientation = orientation;
+	}
+
+	public Object getPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void moveForward() {
+		// TODO Auto-generated method stub
+		
+	}
 }
