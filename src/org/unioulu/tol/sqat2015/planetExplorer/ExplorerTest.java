2261cfcdc8bc92e6022bc9309019ccde9087d072
somename <someemail>
2015-10-23 12:44:02 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/ExplorerTest.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/ExplorerTest.java
@@ -7,13 +7,27 @@ import org.junit.Test;
 public class ExplorerTest {
 
 	@Test
-	public void testMoveForward() {
+	public void testMoveForwardFromNull() {
 	
 		Explorer explorer = new Explorer(0,0,"N");
 		
 		explorer.moveForward();
 		
 		assertEquals("1,0,N", explorer.getPosition());
+		
+	}
+	
+	@Test
+	public void testMoveForwardFromNullTurnRightAndMoveForwardTwice(){
+		
+		Explorer explorer = new Explorer(0,0,"N");
+		
+		explorer.moveForward();
+		explorer.turnRight();
+		explorer.moveForward();
+		explorer.moveForward();
+		
+		assertEquals("1,2,E", explorer.getPosition());
 		
 	}
 
